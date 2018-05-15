4f29a041089374b33da238cbe32e45e40b350ef7
aritradey97 <aritradey97@gmail.com>
2018-05-15 12:14:27 +0530

    5th Commit

--- app/src/main/java/com/btechviral/android/bloodbank/QueryActivity.java
+++ app/src/main/java/com/btechviral/android/bloodbank/QueryActivity.java
@@ -3,7 +3,9 @@ package com.btechviral.android.bloodbank;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -13,6 +15,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -24,7 +27,7 @@ public class QueryActivity extends AppCompatActivity {
     FirebaseUser firebaseUser;
     ArrayList<String> items = new ArrayList<>();
     ArrayList<Item> itemList = new ArrayList<>();
-    String query;
+    String queryText = "B-", queryText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,24 +44,26 @@ public class QueryActivity extends AppCompatActivity {
             startActivity(intent);
         } else {
             final EditText editText = (EditText)findViewById(R.id.queryText);
+            final EditText editText2 = (EditText)findViewById(R.id.queryText2);
             final  ListView listView = (ListView)findViewById(R.id.listView);
             firebaseDatabase = FirebaseDatabase.getInstance().getReference("users");
 
-            query = "B-";
+
 
             final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
             listView.setAdapter(adapter);
-            ValueEventListener valueEventListener = new ValueEventListener() {
+            final ValueEventListener valueEventListener = new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
                     if(dataSnapshot.exists()){
                         for(DataSnapshot snapshot: dataSnapshot.getChildren()){
                             Item item = snapshot.getValue(Item.class);
                             itemList.add(item);
-                            items.add(item.getName());
-                            items.add(item.getPlace());
-                            items.add(item.getMedicHist());
-                            items.add(" ");
+                            items.add("Name: "+ item.getName() + "\n"+"Place: "+ item.getPlace() + "\n"
+                                    + "Medical History: " + item.getMedicHist());
+//                            items.add(item.getPlace());
+//                            items.add(item.getMedicHist());
+//                            items.add(" ");
                         }
                         adapter.notifyDataSetChanged();
                     }
@@ -69,7 +74,22 @@ public class QueryActivity extends AppCompatActivity {
 
                 }
             };
-            firebaseDatabase.child(query).child("items").addListenerForSingleValueEvent(valueEventListener);
+//            if(queryText2 == null){
+//                firebaseDatabase.child(queryText).child("items").addListenerForSingleValueEvent(valueEventListener);
+//            } else {
+//                Query query = FirebaseDatabase.getInstance().getReference("users").child(queryText).orderByChild("place").equalTo(queryText2);
+//                query.addListenerForSingleValueEvent(valueEventListener);
+//            }
+            Button button = (Button)findViewById(R.id.searchButton);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(editText.getText().toString() != null)
+                        queryText = editText.getText().toString();
+                    queryText2 = editText2.getText().toString();
+                    firebaseDatabase.child(queryText).child("items").orderByChild("place").equalTo(queryText2).addListenerForSingleValueEvent(valueEventListener);
+                }
+            });
         }
     }
 }
